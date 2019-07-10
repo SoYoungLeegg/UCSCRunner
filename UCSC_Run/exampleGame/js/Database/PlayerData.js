bef7f279c34c8fc56f660c85eb0e883b2e699d5f
Junhee Heo <hela010955@gmail.com>
2019-07-10 00:30:04 -0700

    Initializing Firebase

---
+++ UCSC_Run/exampleGame/js/Database/PlayerData.js
@@ -0,0 +1,13 @@
+// Firebase configuration for player data
+var firebaseConfig = {
+    apiKey: "AIzaSyCjQ9BQoSdS38bp1fuVeTNp9hg83Zb7jZs",
+    authDomain: "ucscrun.firebaseapp.com",
+    databaseURL: "https://ucscrun.firebaseio.com",
+    projectId: "ucscrun",
+    storageBucket: "",
+    messagingSenderId: "934701608923",
+    appId: "1:934701608923:web:c8eb9691838c47ed"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

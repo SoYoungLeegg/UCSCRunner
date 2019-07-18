dfa95b1904537935ebbf6bd3582f75209182d030
Junhee Heo <hela010955@gmail.com>
2019-07-18 13:57:11 -0700

    change background for scoreboard

--- UCSC_Run/exampleGame/js/States/ScoreBoard.js
+++ UCSC_Run/exampleGame/js/States/ScoreBoard.js
@@ -2,14 +2,16 @@
 
 ScoreBoard.prototype = {
 	preload: function(){
-		game.stage.backgroundColor = "#ffffff";
-		game.load.image('score', 'assets/img/scoretitle.png');
+		//game.stage.backgroundColor = "#ffffff";
+		game.load.image("background", "assets/img/sky.png");
+        game.load.image('score', 'assets/img/scoretitle.png');
 		game.load.image('scorecell', 'assets/img/scorecell.png');
 		game.load.image('restartbutton', 'assets/img/button/restartbutton.png');
 		
 	},
 	create: function(){
-		
+	
+        game.add.tileSprite(0, 0, 1000, 600, 'background');
 		var title = game.add.sprite(250, 100, 'score');
 		
 		/* var submitForm = game.add.sprite(210, 200, 'scorecell').scale.setTo(0.5,0.5); */
